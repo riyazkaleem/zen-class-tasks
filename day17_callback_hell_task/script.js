8b09c89ff1c96043bb34fd0076cda4e2984332f2
riyazkaleem <riyaz.marx@gmail.com>
2021-11-20 15:33:52 +0530

    Create script.js

---
+++ day17_callback_hell_task/script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.querySelector(".container").innerHTML = "T minus 10";
+  setTimeout(() => {
+    document.querySelector(".container").innerHTML = "T minus 9";
+    setTimeout(() => {
+      document.querySelector(".container").innerHTML = "T minus 8";
+      setTimeout(() => {
+        document.querySelector(".container").innerHTML = "T minus 7";
+        setTimeout(() => {
+          document.querySelector(".container").innerHTML = "T minus  6";
+          setTimeout(() => {
+            document.querySelector(".container").innerHTML = "T minus 5";
+            setTimeout(() => {
+              document.querySelector(".container").innerHTML = "T minus 4";
+              setTimeout(() => {
+                document.querySelector(".container").innerHTML = "T minus 3";
+                setTimeout(() => {
+                  document.querySelector(".container").innerHTML = "T minus 2";
+                  setTimeout(() => {
+                    document.querySelector(".container").innerHTML =
+                      "T minus 1";
+                    setTimeout(() => {
+                      document.querySelector(".container").innerHTML = "LAUNCH";
+                    }, 2000);
+                  }, 2000);
+                }, 2000);
+              }, 2000);
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
